33f19c4fe2221f7c7be3ba9e5e593fb87afb232e
Timur Lii <timurka260197@gmail.com>
2024-12-04 09:43:27 +0200

    Add file migration

---
+++ src/main/resources/db/migration/V4_Insert_data_manager.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO managers (manager_name)
+-- VALUES
+--     ('Manager 1'),
+--     ('Manager 2'),
+--     ('Manager 3'),
+--     ('Manager 4'),
+--     ('Manager 5'),
+--     ('Manager 6'),
+--     ('Manager 7'),
+--     ('Manager 8');
